3ef4c515c6ac905c40d0c1aa2dd9893e84a95d07
Jared Clemmensen and russell hofvendahl <jared@>
2018-02-08 16:20:32 -0800

    add files test and method

--- lib/ruby_project.rb
+++ lib/ruby_project.rb
@@ -3,8 +3,14 @@ require 'pry'
 require 'fileutils'
 
 class Builder
+  include FileUtils
+
   def initialize(project_name)
     @project_name = project_name
+  end
+
+  def clean
+    FileUtils.rm_rf("../#{@project_name}")
   end
 
   def directories
@@ -13,7 +19,18 @@ class Builder
     FileUtils.mkdir("../#{@project_name}/spec")
   end
 
-  def clean
-    FileUtils.rm_rf("../#{@project_name}")
+  def files
+    FileUtils.touch("../#{@project_name}/lib/#{@project_name}.rb")
+    FileUtils.touch("../#{@project_name}/spec/#{@project_name}_spec.rb")
+    FileUtils.touch("../#{@project_name}/Gemfile")
+    FileUtils.touch("../#{@project_name}/#{@project_name}_script.rb")
+    FileUtils.touch("../#{@project_name}/README.md")
+  end
+
+  def content
+    
+    lib_file = File.open("../#{@project_name}/lib/#{@project_name}.rb", "w")
+    lib_file.puts('#!/usr/bin/env ruby')
+    lib_file.close
   end
 end
